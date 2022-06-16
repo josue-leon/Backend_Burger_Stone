1273e605db4a61f6d962084bb746b8483254b8af
unknown <eli.vizuete@gmail.com>
2022-06-16 08:50:11 -0500

    Add conex Andrea

--- server.js
+++ server.js
@@ -7,6 +7,14 @@ const server = http.createServer(app);
 const port = process.env.PORT || 3000;
 app.set('port', port);
 
+//JOSUE
+/*
 server.listen(3000, '192.168.0.120' || 'localhost', function(){
+    console.log('Aplicacion de NodeJs ' +port + ' Iniciada..')
+});
+*/
+
+//ANRDEA
+server.listen(3000, '192.168.100.15' || 'localhost', function(){
     console.log('Aplicacion de NodeJs ' +port + ' Iniciada..')
 });
